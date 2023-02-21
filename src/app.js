32b45ce49b3e6c54514dd3069447d200b1827ab2
RichardoTS <ri.tapiasaez@gmail.com>
2023-02-21 19:44:26 +0000

    Minor changes

--- src/app.js
+++ src/app.js
@@ -8,14 +8,15 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   // for (let i = 0; i < 10; i++) {
   //   document.getElementByClass("container").innerHTML = [i];
-
-  console.log("Hello Rigo from the console!");
+  /* console.log("Hello Rigo from the console!"); */
 };
 function getRandomInt() {
   let randomNumber = Math.floor(Math.random() * 10) + 1;
   return randomNumber;
 }
-console.log(getRandomInt());
+/* console.log(getRandomInt()); */
+
+middleNum.innerHTML = getRandomInt();
 // for (let numbrs = 1; numbrs <= 10; numbrs++) {
 //   console.log("these are the numbers " + numbrs);
 // }
@@ -25,3 +26,28 @@ console.log(getRandomInt());
 setTimeout(() => {
   document.location.reload();
 }, 10000);
+
+function getSuit(suitNumber = 0) {
+  //make sure parameter is a number and not a string by converting the value to int:
+
+  switch (suitNumber) {
+    case 1:
+      return "♦";
+    case 2:
+      return "♥";
+    case 3:
+      return "♠";
+    case 4:
+      return "♣";
+  }
+}
+function getSuits() {
+  // for (let i = 0; i < 4; i++) {
+  let theSuit = getSuit(Math.floor(Math.random() * 4) + 1);
+  return theSuit;
+  // }
+}
+console.log(getSuits());
+let result = getSuits();
+upperSuit.innerHTML = result;
+bottmSuit.innerHTML = result;
