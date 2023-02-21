ed2bbdd172b39f91430c5d161a2eeaa824a8da91
RichardoTS <ri.tapiasaez@gmail.com>
2023-02-21 18:05:59 +0000

    Saving progress

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  // for (let i = 0; i < 10; i++) {
+  //   document.getElementByClass("container").innerHTML = [i];
+
   console.log("Hello Rigo from the console!");
 };
+for (let suit = 1; suit <= 4; suit++) {
+  console.log("these are the suits" + suit);
+}
+for (let numbrs = 1; numbrs <= 10; numbrs++) {
+  console.log("these are the numbers " + numbrs);
+}
+/* 
+    La página se refresca cada 10 segundos
+*/
+setTimeout(() => {
+  document.location.reload();
+}, 10000);
