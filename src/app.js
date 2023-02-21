26cd16f6abf93fbffbba5425d04a761a5abc4aae
RichardoTS <ri.tapiasaez@gmail.com>
2023-02-21 20:50:35 +0000

    Project Finished

--- src/app.js
+++ src/app.js
@@ -5,31 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  // for (let i = 0; i < 10; i++) {
-  //   document.getElementByClass("container").innerHTML = [i];
-  /* console.log("Hello Rigo from the console!"); */
-};
+window.onload = function() {};
 function getRandomInt() {
   let randomNumber = Math.floor(Math.random() * 10) + 1;
   return randomNumber;
 }
-/* console.log(getRandomInt()); */
 
 middleNum.innerHTML = getRandomInt();
-// for (let numbrs = 1; numbrs <= 10; numbrs++) {
-//   console.log("these are the numbers " + numbrs);
-// }
-/* 
-    La página se refresca cada 10 segundos
-*/
+
+//  La página se refresca cada 10 segundos
 setTimeout(() => {
   document.location.reload();
 }, 10000);
 
 function getSuit(suitNumber = 0) {
-  //make sure parameter is a number and not a string by converting the value to int:
-
   switch (suitNumber) {
     case 1:
       return "♦";
@@ -42,12 +31,21 @@ function getSuit(suitNumber = 0) {
   }
 }
 function getSuits() {
-  // for (let i = 0; i < 4; i++) {
   let theSuit = getSuit(Math.floor(Math.random() * 4) + 1);
   return theSuit;
-  // }
 }
 console.log(getSuits());
 let result = getSuits();
+
+if (result == "♦" || result == "♥") {
+  upperSuit.style.color = "red";
+  bottmSuit.style.color = "red";
+  middleNum.style.color = "red";
+} else {
+  upperSuit.style.color = "black";
+  bottmSuit.style.color = "black";
+  middleNum.style.color = "black";
+}
+
 upperSuit.innerHTML = result;
 bottmSuit.innerHTML = result;
